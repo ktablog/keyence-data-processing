9e7d058ccdee578f0157a41ed1154dbd197ef8e4
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2021-08-10 14:54:43 +0500

    project started

---
+++ KeyenceDataProcessing/KeyenceDataProcessing/CfgFile.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.IO;
+
+namespace KeyenceDataProcessing
+{
+    class CfgFile
+    {
+        private Dictionary<string, string> _dictionary = new Dictionary<string,string>();
+
+        void Clear()
+        {
+            _dictionary.Clear();
+        }
+       
+
+        internal void Load(string fileName)
+        {
+            try
+            {
+                String text;
+                using (StreamReader reader = new StreamReader(fileName, Encoding.UTF8))
+                {
+                    text = reader.ReadToEnd();
+                }
+                string[] lines = text.Split(new string[] {Environment.NewLine},
+                                            StringSplitOptions.RemoveEmptyEntries);
+                foreach (string scanLine in lines)
+                {
+                    int index = scanLine.IndexOf('=');
+                    if (index > 0)
+                    {
+                        string key = scanLine.Substring(0, index).Trim();
+                        string value = scanLine.Substring(index + 1).Trim();
+                        write(key, value);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+
+        internal void Save(string fileName)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> pair in _dictionary)
+            {
+                stringBuilder.Append(pair.Key)
+                             .Append("=")
+                             .Append(pair.Value)
+                             .Append(Environment.NewLine);                
+            }
+
+            byte[] textData = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+
+            using (FileStream stream = new FileStream(fileName, FileMode.Create))
+            {
+                stream.Write(textData, 0, textData.Length);
+            }
+        }
+
+
+        internal void write(string key, string value)
+        {
+            _dictionary[key] = value;
+        }
+
+
+        internal void write(string key, bool value)
+        {
+            write(key, value.ToString(CultureInfo.InvariantCulture));
+        }
+
+
+        internal void write(string key, int value)
+        {
+            write(key, value.ToString(CultureInfo.InvariantCulture));
+        }
+
+
+        internal void write(string key, double value)
+        {
+            write(key, value.ToString(CultureInfo.InvariantCulture));
+        }
+
+
+        internal string read(string key, string defValue)
+        {
+            string result = defValue;
+            try
+            {
+                result = _dictionary[key];
+            }
+            catch
+            {
+            }
+            return result;
+        }
+
+
+        internal bool read(string key, bool defValue)
+        {
+            bool result = defValue;
+            try
+            {
+                result = Boolean.Parse(read(key, ""));
+            }
+            catch
+            {
+            }
+            return result;
+        }
+
+
+        internal int read(string key, int defValue)
+        {
+            int result = defValue;
+            try
+            {
+                result = Int32.Parse(read(key, ""), CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+            return result;
+        }
+
+
+        internal double read(string key, double defValue)
+        {
+            double result = defValue;
+            try
+            {
+                result = Double.Parse(read(key, ""), CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+            }
+            return result;
+        }
+    }
+}
+
+
---
+++ KeyenceDataProcessing/KeyenceDataProcessing/Common.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace KeyenceDataProcessing
+{
+    static class Common
+    {
+        private const string _applicationName = "KeyenceDataProcessing";
+        private const string _configFileName = _applicationName + ".config";
+        private static string _configPath;
+
+        private const string _keyenceKeyPrefix = "Keyence";
+        private const string _keyenceIpKey = _keyenceKeyPrefix + "Ip";
+        private const string _keyencePortKey = _keyenceKeyPrefix + "Port";
+        private const string _simaticKeyPrefix = "Simatic";
+        private const string _simaticIpKey = _simaticKeyPrefix + "Ip";
+        private const string _simaticBlockKey = _simaticKeyPrefix + "Block";
+        private const string _simaticResultYAddressKey = _simaticKeyPrefix + "ResultYAddress";
+        private const string _simaticResultZAddressKey = _simaticKeyPrefix + "ResultZAddress";
+        private const string _simaticQualityAddressKey = _simaticKeyPrefix + "QualityAddress";
+        private const string _simaticCounterAddressKey = _simaticKeyPrefix + "ConterAddress";
+       
+        public static string KeyenceIp { get; set; }
+        public static string KeyencePort { get; set; }
+        public static string SimaticIp { get; set; }
+        public static string SimaticBlock { get; set; }
+        public static string SimaticResultYAddress { get; set; }
+        public static string SimatciResultZAddress { get; set; }
+        public static string SimaticQualityAddress { get; set; }
+        public static string SimaticCounterAddress { get; set; }
+
+        internal static string configPath
+        {
+            get
+            {
+                return _configPath;
+            }
+        }
+
+
+        static Common() 
+        {
+            _configPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) +
+                Path.DirectorySeparatorChar + _applicationName;
+        }
+
+
+        internal static void load()
+        {
+            CfgFile cfg = new CfgFile();
+
+            cfg.Load(configPath + Path.DirectorySeparatorChar + _configFileName);
+        }
+
+
+        internal static void save()
+        {
+            try
+            {
+                if (!Directory.Exists(_configPath))
+                {
+                    Directory.CreateDirectory(_configPath);
+                }
+
+                CfgFile cfg = new CfgFile();
+
+                cfg.Save(_configPath + Path.DirectorySeparatorChar + _configFileName);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
