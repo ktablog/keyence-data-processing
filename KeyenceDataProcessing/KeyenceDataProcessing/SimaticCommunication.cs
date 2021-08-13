e33cca7fd95483c3aa7e42a79ed01052f20d81ab
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2021-08-13 15:13:42 +0500

    adding simatic communication

---
+++ KeyenceDataProcessing/KeyenceDataProcessing/SimaticCommunication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Sharp7;
+
+namespace KeyenceDataProcessing
+{
+    class SimaticCommunication
+    {
+        private Thread _thread = null;
+        private volatile bool _terminate = true;
+
+        public void Start()
+        {
+            Stop();
+
+            if (Open())
+            {
+                ThreadStart threadStart = new ThreadStart(this.Run);
+                _thread = new Thread(threadStart);
+                _terminate = false;
+                _thread.Start();
+            }
+        }
+
+
+        public void Stop()
+        {
+            _terminate = true;
+            _thread = null;
+
+            Close();
+        }
+
+
+        public void Run()
+        {
+            Console.Out.WriteLine("Thread started");
+            while (!_terminate)
+            {
+                Thread.Sleep(1000);
+            }
+            Console.Out.WriteLine("Thread stopped");
+        }
+
+
+        private bool Open()
+        {
+            return true;
+        }
+
+
+        private void Close()
+        {
+        }
+    }
+}
